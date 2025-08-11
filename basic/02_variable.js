857c998889cb000831244e4eec3da0ec520beab2
sunil jangid <132828535+snljgd@users.noreply.github.com>
2025-08-11 09:06:21 +0000

    data types

--- basic/02_variable.js
+++ basic/02_variable.js
@@ -7,4 +7,14 @@
     4); // code readability should be high
 
 console.log("bangluru")
- 
+
+// number = 2 to power 53
+// bigint 
+// string
+// boolean = true / false 
+// null = standalone value
+// undefined 
+// symbol == unique
+
+
+console.log(typeof null);
