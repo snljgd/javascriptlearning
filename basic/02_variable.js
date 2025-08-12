a98c7a22dc5547a6e5991b3cfadd824d91501903
sunil jangid <132828535+snljgd@users.noreply.github.com>
2025-08-12 15:47:26 +0000

    string

--- basic/02_variable.js
+++ basic/02_variable.js
@@ -8,6 +8,8 @@
 
 console.log("bangluru")
 
+    // 1. Primitive data type 
+
 // number = 2 to power 53
 // bigint 
 // string
@@ -15,8 +17,51 @@ console.log("bangluru")
 // null = standalone value
 // undefined 
 // symbol == unique
+// BigInt
 
+
+    // 2. Reference data type (non primitive data type) 
+        // Array 
+        // object 
+        // Function  
 
 console.log(typeof null);
 window.alert("Hello world!");
 alert("Hello world!");
+
+
+// memory management system  --------------
+
+// java script memory mainly two types 
+
+// 1. stack  (small fixed size data (primitive data types)store karne me )
+// 2. heap   (big dynamic size data (object arrays functions  store karne me))
+
+// 1.stack (primitive values) 
+
+// data direct values ke rup me store hota Headers 
+// primitive data types :- number string Boolean null undefined symbol bigint 
+
+    // 2. Heap Memory ( Reference valaues) 
+
+    // large and dynamic storage 
+
+    // data reference (pointer) ke jariye access hota he 
+
+    // non primitive types :- object array functions data 
+
+
+// Garbage Collection 
+
+// java script me automatic garbage collection hota he 
+
+// algorithm : mark and sweep 
+
+// ager koi object reachable nhi he ( koi reference nhi becha to memory free ho jati he )
+
+// let users = { name :"sunil"};
+
+// user = null ; ( object unreachable -> Garbage Collector free kerega)
+
+
+
---
+++ basic/06_String.js
@@ -0,0 +1,17 @@
+// sting ********************
+
+const name = "JAIPUR"
+
+const citycode = 14
+
+
+console.log(`my city name is ${name} and my city code${citycode}`);
+
+
+const cityName = new String('bangluru')
+
+console.log(cityName.charAt(3));
+
+console.log(cityName.blink())
+
+
