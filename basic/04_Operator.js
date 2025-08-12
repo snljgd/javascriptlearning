3432711cd9a5da274d7d276c6e99ad6e9fd6aba9
sunil jangid <132828535+snljgd@users.noreply.github.com>
2025-08-12 08:17:47 +0000

    comparsion

---
+++ basic/04_Operator.js
@@ -0,0 +1,29 @@
+// *********java script operator **************
+
+// console.log(2+2); 
+// console.log(3-2);
+// console.log(2*3);
+// console.log(4/2);
+// console.log(7%2);
+
+
+// console.log("1"+2) first value string he to operator left to right chelta he to aage ki value ko bi string me convert kar dega 
+
+// console.log(1+"2");  
+// console.log("2" + 3 +3);
+
+// console.log(2+4+"4");    left to right phele add operator hoga fir string add hogi 
+
+// prefix or postfix 
+
+// let x =5
+// console.log(++x)  phele increment huaa fir print huaa 
+
+
+// x = 5
+
+// console.log(x++); phele print fir increment 
+
+
+
+
---
+++ basic/05_comparison.js
@@ -0,0 +1,38 @@
+// console.log("2">1);  true
+
+// console.log("02" == 2);  true
+
+// console.log(true == 1);  true
+ 
+// console.log(false ==0);  true
+
+// console.log(null == 0);  false
+
+// console.log(null>=0);   true
+
+// console.log(undefined == 0 ); false
+
+
+// console.log(null>0); JS convert null to number - 0 check if 0>0 = false
+
+// console.log(null == 0); == me null only equal undefined so null == 0 - false
+
+// console.log(null >= 0); >= comparison me null ko 0 me convert karta he so 0>=0 - true
+
+// console.log(undefined == 0); undefined only loosely equal for null so undefined == 0 - false 
+
+// console.log(undefined > 0); false 
+
+// console.log(undefined<0); false 
+
+
+
+
+
+
+
+
+
+
+
+
