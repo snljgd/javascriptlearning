ea24ae1a039b560e34b8707d33896180fa04d7b8
sunil jangid <132828535+snljgd@users.noreply.github.com>
2025-08-13 09:42:11 +0000

    number

--- basic/07_numbermaths.js
+++ basic/07_numbermaths.js
@@ -1,13 +1,22 @@
-const score = 400
-console.log(score)
+// const score = 400
+// console.log(score)
 
 
-const balance = 100
+// const balance = 100
 
-console.log(balance.toString().length)
+// console.log(balance.toString().length)
 
-console.log(balance.toFixed(1))
+// console.log(balance.toFixed(1))
 
 
-const number= 1252.4999
-console.log(number.toPrecision(4))
+// const number= 1252.4999
+// console.log(number.toPrecision(4)) round of karta he 
+
+// const handreds = 1000000
+console.log(handreds.toLocaleString('en-IN'))
+
+
+
+// *****************MATHS**********************
+
+
