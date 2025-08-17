ce42515271fe1b201f9666e3f72b89c6a48df030
sunil jangid <132828535+snljgd@users.noreply.github.com>
2025-08-17 05:56:15 +0000

    array

---
+++ basic2/array.js
@@ -0,0 +1,21 @@
+//array
+
+
+const Myarray = new Array(2,6,3,5,5)
+console.log(Myarray[3])
+
+let arr = [2 , "jaipur" , true]
+
+console.log(arr)
+
+
+let arra = "sunil"
+let str = Array.from(arra)
+console.log(str)
+console.log(str.length)
+
+
+let fruit = ["apple" , "banana" , "orange"]
+fruit [2] = "graps"
+
+console.log(fruit);
