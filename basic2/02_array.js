3bcfebe8e040bc5c7a024f9f5f0309d66ba1c827
sunil jangid <132828535+snljgd@users.noreply.github.com>
2025-09-04 11:35:39 +0000

    comt

---
+++ basic2/02_array.js
@@ -0,0 +1,11 @@
+const array = [5,4,3,2,1]
+array.reverse()
+// console.log(array);
+// console.log(array)
+
+
+const array1 = [1,2,3,[4,5,6,[7,8,9]]]
+
+const array2 = array1.flat(Infinity)
+
+console.log(array2)
